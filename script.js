e28c51de63942ede758138cd036eeadbd909cc9e
sheryiscool <liveclass@sheryians.org>
2023-08-07 22:07:58 +0530

    Page5 done

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-gsap.registerPlugin(ScrollTrigger);
+function loco(){
+  gsap.registerPlugin(ScrollTrigger);
 
 // Using Locomotive Scroll from Locomotive https://github.com/locomotivemtl/locomotive-scroll
 
@@ -29,9 +30,23 @@ ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
 // after everything is set up, refresh() ScrollTrigger and update LocomotiveScroll because padding may have been added for pinning, etc.
 ScrollTrigger.refresh();
 
-
-
-
+}
+function swiperWorking(){
+  var swiper = new Swiper(".mySwiper", {
+    slidesPerView: "auto",
+    pagination: {
+      el: ".swiper-pagination",
+      type:"fraction"
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+  });
+  
+}
+loco()
+swiperWorking()
 var tl = gsap.timeline();
 tl.from("#page1 svg", {
   y: -40,
@@ -86,3 +101,27 @@ gsap.to("#page2 #svg2,#page2 #svg3",{
     scrub:2,
   }
 })
+
+var tl2 = gsap.timeline({
+  scrollTrigger:{
+    trigger:"#page5-left",
+    scroller:"#main",
+    start:"top 50%",
+    end:"top 35%",
+    scrub:3,
+    // markers:true
+  }
+})
+tl2.to("#page5-left",{
+  transform:"translateX(-20%)",
+  duration:1,
+},"anim2")
+tl2.to("#page5-right",{
+  transform:"translateX(20%)",
+  duration:1,
+},"anim2")
+tl2.from("#page5-center",{
+  transform:"translateY(30%)",
+  duration:1,
+  opacity:0
+},"anim2")
